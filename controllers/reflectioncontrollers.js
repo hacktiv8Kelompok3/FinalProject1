42f0f0b986b29ee31ddddc40f3b4398cdace29c8
AndreasNugroho <andreas.nugroho@ti.ukdw.ac.id>
2023-04-22 10:27:26 +0700

    add create reflection

--- controllers/reflectioncontrollers.js
+++ controllers/reflectioncontrollers.js
@@ -3,15 +3,19 @@ const db = require('../config/connection')
 class reflectioncontroller { 
     static async createReflection(req, res) {
         try {
+            const { id } = req.UserData
             const { success, low_point, take_away } = req.body;
             if (!success || !low_point || !take_away) {
                 throw {
-                    code: 401,
-                    message: 'invalid'
+                    code: 400,
+                    message: "required",
                 }
             }
-            console.log(success)
-            res.status(200).json("oke")
+            const insertData = await db.query(
+                'INSERT INTO reflections (success,low_point,take_away,userid) VALUES ($1,$2,$3,$4) RETURNING *',
+                [String(success), String(low_point), String(take_away),id]
+            )
+            res.status(200).json(insertData.rows[0])
         } catch (error) {
             res.status(error?.code || 500).json(error)
         }
