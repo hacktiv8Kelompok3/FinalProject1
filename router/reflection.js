8e0e07eb9b0e6864483627ea9e44aa3d0b772466
Nazrin- <jihadi013@gmail.com>
2023-04-24 15:08:13 +0800

    add update route

--- router/reflection.js
+++ router/reflection.js
@@ -4,5 +4,6 @@ const reflection = require('../controllers/reflectioncontrollers')
 
 router.post('/', reflection.createReflection)
 router.get('/', reflection.getReflection)
+router.put('/',reflection.updateReflection)
 
 module.exports = router
