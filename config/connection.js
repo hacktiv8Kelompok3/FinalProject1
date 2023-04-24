eb6b2d9cf1efeb0ca1ced40e26ee22935fde0310
Nazrin- <jihadi013@gmail.com>
2023-04-24 11:32:46 +0800

    Commit Pertama

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ const Pool = require("pg").Pool;
 const config = new Pool({
   user: "postgres",
   host: "localhost",
-  database: "finalproject1",
-  password:"1911andre",
-  port: 5432,
+  database: "reflections",
+  password:"admin",
+  port: 3333,
 });
 config.connect(err => {
     if (err) {
